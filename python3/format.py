7c93d091cf1545a81fff0f52a599a2d00ea2f26e
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 00:53:19 +0300

    add some code

--- python3/format.py
+++ python3/format.py
@@ -1,6 +1,8 @@
 name = 'Andrew'
 age = 21
 
+# https://www.python.org/dev/peps/pep-3101/
 print('my name is {}. I have {} eyars'.format(name, age))
 print('my name is {0}. I have {1} eyars'.format(name, age))
+('my name is {name}. I have {age} eyars'.format(name = 'Andrew', age = 21))
 print('my name is ' + name + '. I have ' + str(age) + ' eyars')
