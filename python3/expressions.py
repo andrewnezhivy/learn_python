ef79c12ebff59d0fb5d8719990a837c843d8d2c4
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 02:16:21 +0300

    add if.py

--- python3/expressions.py
+++ python3/expressions.py
@@ -1,2 +1,10 @@
+# https://docs.python.org/3/reference/expressions.html#summary
 a = 2 ; a = a * 3
 a = 2 ; a *= 3
+
+length = 5
+breadth = 2
+
+area = length * breadth
+print('area equals', area)
+print('perimeter equals', 2 * (length + breadth))
