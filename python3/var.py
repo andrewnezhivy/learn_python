68c02a699462a2a79ca69dd27a695aa7d4e7ef09
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 01:45:38 +0300

    adsome code

--- python3/var.py
+++ python3/var.py
@@ -1,12 +1,15 @@
 i = 5
-
 print(i)
 
-i = i + i
-
+i = i + 1
 print(i)
 
-s = '''First line.
-Second line.'''
+s = '''first line.
+second line.'''
+print(s)
+s = 'first \
+line'
 
 print(s)
+print\
+(s)
