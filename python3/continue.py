1013085e902e92b1806f55f3cc49afd3bb44e122
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 04:11:26 +0300

    add continue.py

---
+++ python3/continue.py
@@ -0,0 +1,12 @@
+while True:
+	s = input('enter something: ')
+	
+	if s == 'exit':
+		break
+	print('length of string:', len(s))
+	
+	if len(s) < 3:
+		print('too few')
+		continue
+	
+	print('entered string a sufficient length')
