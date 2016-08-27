56be3d707795c90b74ac8a4d99e7f47847178792
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 03:25:25 +0300

    add some code

---
+++ python3/while.py
@@ -0,0 +1,19 @@
+number = 23
+running = True
+
+while running:
+	guess = int(input('please enter an integer: '))
+
+	if guess == number:
+		print('сongratulations, you guessed it')
+		print('(although it did not win any prize)')
+		
+		running = False
+	elif guess < number:
+		print('no, a little more unknown number of')
+	else:
+		print('no, a little less unknown number of')
+else:
+	print('while loop completed')
+
+print('completed')
