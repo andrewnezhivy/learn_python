af0a01052210f955d35ae1393153ce1a4776c282
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 23:03:53 +0300

    add some code

--- python3/func_param.py
+++ python3/func_param.py
@@ -1,10 +1,10 @@
 def printMax(a, b):
-	if a > b:
-		print(a, 'is the maximum')
-	elif a == b:
-		print(a, 'equals', b)
-	else:
-		print(b, 'is the maximum')
+    if a > b:
+        print(a, 'is the maximum')
+    elif a == b:
+        print(a, 'equals', b)
+    else:
+        print(b, 'is the maximum')
 
 printMax(3, 4)
 
