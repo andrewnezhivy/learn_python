aba060c46495b50548246cefcce7697b1bcfa2f6
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 03:15:22 +0300

    add while.py

---
+++ python3/if.py
@@ -0,0 +1,12 @@
+number = 23
+guess = int(input('please enter an integer: '))
+
+if guess == number:
+	print('сongratulations, you guessed it')
+	print('(although it did not win any prize)')
+elif guess < number:
+	print('no, a little more unknown number of')
+else:
+	print('no, a little less unknown number of')
+
+print('completed')
