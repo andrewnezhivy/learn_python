ab69cbb519508b62e839dc60c928c04d3a1f0cc1
Andrew Nezhivy <andrew.nezhivy@yandex.com>
2016-08-27 04:37:40 +0300

    add func_param.py

---
+++ python3/func_param.py
@@ -0,0 +1,14 @@
+def printMax(a, b):
+	if a > b:
+		print(a, 'is the maximum')
+	elif a == b:
+		print(a, 'equals', b)
+	else:
+		print(b, 'is the maximum')
+
+printMax(3, 4)
+
+x = 5
+y = 7
+
+printMax(x, y)
